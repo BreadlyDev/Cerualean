5105c5a14675d259935139e06728629a1cfba67e
BreadlyDev <breadydev@gmail.com>
2025-02-18 21:44:52 +0600

    Added swagger and DB config

--- Program.cs
+++ Program.cs
@@ -1,6 +1,28 @@
+using Cerualean.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "Cerulean", Version = "v1" });
+});
+builder.Services.AddDbContext<ApplicationDBContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseSwagger();
+app.UseSwaggerUI(options => 
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "Cerulean v1");
+    options.RoutePrefix = string.Empty;
+});
+app.MapControllers();
 
 app.Run();
