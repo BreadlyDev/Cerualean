9a7390aac10bc903ce9749f3ef94d02de42d0471
BreadlyDev <breadydev@gmail.com>
2025-02-21 11:43:53 +0600

    Added Common Exception messages

---
+++ Domain/Common/Exceptions/ExceptionMessages.cs
@@ -0,0 +1,8 @@
+namespace Cerualean.Domain.Common.Exceptions
+{
+    public static class ExceptionMessages
+    {
+        public static readonly string InternalServerError
+        = "Internal server error";        
+    }
+}
