49ddbb7c8c9219b1773d561960ce5cf19bc3a1a0
BreadlyDev <breadydev@gmail.com>
2025-02-23 00:25:44 +0600

    Updated Exception message name

--- Domain/Modules/Courses/Helpers/CourseExceptionMessages.cs
+++ Domain/Modules/Courses/Helpers/CourseExceptionMessages.cs
@@ -2,7 +2,7 @@ namespace Cerualean.Domain.CourseModule.Helpers
 {
     public static class CourseExceptionMessages
     {
-        public static readonly string CourseNotFoundError 
+        public static readonly string CourseNotFound 
         = "Course not found"; 
     }
 }
