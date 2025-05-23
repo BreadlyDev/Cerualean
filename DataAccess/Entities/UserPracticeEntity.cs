3290a9aefb9c0b31838b1c338089ff66caeade32
BreadlyDev <breadydev@gmail.com>
2025-05-23 22:41:09 +0600

    Fixed UserPracticeEntity

--- DataAccess/Entities/UserPracticeEntity.cs
+++ DataAccess/Entities/UserPracticeEntity.cs
@@ -4,8 +4,15 @@ public class UserPracticeEntity
 {
     public int PracticeId { get; set; }
     public PracticeEntity Practice { get; set; }
-	
+
     public int UserId { get; set; }
     public UserEntity User { get; set; }
+
+    public string Code { get; set; }
+    public string? Output { get; set; }
+    public string? Error { get; set; }
+    public bool IsCorrect { get; set; }
+
+    public DateTime AttemptTime { get; set; } = DateTime.UtcNow;
 }
 
